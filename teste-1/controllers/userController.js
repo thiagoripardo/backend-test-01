d32a1f8412dd08ddfd2db04979af48472681e3e1
Thiago Ripardo <tmripardo@gmail.com>
2019-10-11 12:44:05 -0300

    User list done

--- teste-1/controllers/userController.js
+++ teste-1/controllers/userController.js
@@ -1,6 +1,36 @@
+const utils = require('../utils/utils');
+
 // Display list of all Users.
 exports.userList = (req, res) => {
-    res.json({user: 'test'});
+    let responseData = [];
+    let users = utils.getUsers();
+
+    // Simulates a query to a model or database. Also, formats the result.
+    users.forEach(user => {
+        let role = utils.getRoleById(user.role_id)
+        responseData.push({
+            name: user.user_name,
+            role: role ? role.role_name : '',
+            modules: role ? utils.getModulesByRoleId(role.id): []
+        })
+    })
+
+    // Filter
+    if(req.query.role) {
+        responseData = utils.filterByKey(responseData, 'role', req.query.role)
+    } 
+
+    // Sort
+    if(req.query.orderBy && req.query.order){
+        responseData = utils.sortByKey(responseData, req.query.orderBy, req.query.order)
+    }
+
+    // Pagination
+    if(req.query.pageSize && req.query.pageNumber){
+        responseData = utils.paginate(responseData, parseInt(req.query.pageSize), parseInt(req.query.pageNumber))
+    }
+    
+    res.send(responseData);
 }
 
 // Display detail page for a specific User.
--- teste-1/utils/utils.js
+++ teste-1/utils/utils.js
@@ -1,23 +1,35 @@
-const analisys = require('../../assets/analysis');
+const analisys = require('../../assets/analysis.json');
+const users = require('../../assets/user.json');
+const clients = require('../../assets/client.json');
+const roles = require('../../assets/role.json');
+const modules = require('../../assets/module.json');
+const moduleClient = require('../../assets/module-client.json');
+const moduleGroup = require('../../assets/module-group.json');
+const moduleRole = require('../../assets/module-role.json');
 
+// generate a json in chart format
 const generateUnitaryChartFormat = (name, array) => {
-    
-    return {name: name, data: array};
+
+    return {
+        name: name,
+        data: array
+    };
 }
 
+// generate an array in table format
 const generateUnitaryTableFormat = (name, array) => {
     let responseArray = [];
     array.forEach(element => {
         responseArray.push({
             var: name,
-            sample_time: element[0], 
-		    value: element[1]
+            sample_time: element[0],
+            value: element[1]
         })
     });
     return responseArray;
 }
 
-// Simulate getting an analisys
+// Simulate getting analisys
 const getAnalisys = () => {
     let responseJson = {}
     responseJson['1'] = {
@@ -27,9 +39,11 @@ const getAnalisys = () => {
     return responseJson;
 }
 
+
+// Simulate getting an analisys by id
 const getAnalisysById = (id) => {
     let responseJson = {}
-    if(typeof responseJson[id] !== 'function' && !isNaN(parseInt(id))) {
+    if (typeof responseJson[id] !== 'function' && !isNaN(parseInt(id))) {
         responseJson[id] = {
             chart: generateUnitaryChartFormat(`var_${id}`, analisys),
             table: generateUnitaryTableFormat(`var_${id}`, analisys)
@@ -40,9 +54,66 @@ const getAnalisysById = (id) => {
     return responseJson;
 }
 
+// Simulate getting users
+const getUsers = () => {
+    return users;
+}
+
+// Simulate getting a role by id
+const getRoleById = (id) => {
+    let role = roles.filter(element => (element.id === id));
+    return role[0];
+}
+
+// Simulate getting modules by role id
+const getModulesByRoleId = (id) => {
+    let filteredModules = []
+    let filteredModuleRole = moduleRole.filter(element => (element.role_id === id));
+    // console.log(filteredModuleRole)
+    filteredModuleRole.forEach(element => {
+        filteredModules = filteredModules.concat(modules
+            .filter(_module => (_module.id === element.module_id))
+            .map(_module => ({id: _module.id, description: _module.name})))
+    })
+    
+    return filteredModules;
+}
+
+// Filter an array
+const filterByKey = (array, key, value) => {
+    return array.filter(element => (element[key] == value));
+}
+
+// Sort an array
+const sortByKey = (array, key, order) => {
+    return array.sort((a, b) => {
+        var x = a[key];
+        var y = b[key];
+        if (order == 'asc') {
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        } else if (order == 'desc') {
+            return ((x < y) ? 1 : ((x > y) ? -1 : 0));
+        } else {
+            return 0;
+        }
+    });
+}
+
+// Paginate an array (pageNumber starts at 1)
+const paginate = (array, pageSize, pageNumber) => {
+    --pageNumber;
+    return array.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize);
+}
+
 module.exports = {
     generateUnitaryChartFormat,
     generateUnitaryTableFormat,
     getAnalisys,
-    getAnalisysById
+    getAnalisysById,
+    getUsers,
+    getRoleById,
+    getModulesByRoleId,
+    filterByKey,
+    sortByKey,
+    paginate
 }
